f44a527c439a6e6b191ff2d0f03710f9cd456aa9
Izhan Jamal <82833812+IzhanJamal@users.noreply.github.com>
2022-01-24 21:57:50 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Task = ({item}) => {
+    const {title, desc, isComp} = item;
+    return (
+
+        <View style={styles.items}>
+
+               <View style={styles.itemleft}>
+                 <View style={styles.square}></View>
+                 <Text style={styles.itemText}>{title}, {desc}</Text>
+               </View>
+               
+               <TouchableOpacity style={{size:27}}
+                onPrss={() => {
+                    toggComplete();
+                    navigation.goBack();
+                    }}>
+
+               </TouchableOpacity>
+        </View>
+    )
+
+    const toggComplete = async () => {
+        const data = { 'desc': `${desc}`, 'isComp': `${!isComp}` }
+        try {
+            const jsonData = JSON.stringify(data)
+            await AsyncStorage.setItem(title, jsonData)
+        } catch (e) {
+        }
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+const styles= StyleSheet.create({
+items :{
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'space-between',
+    marginTop : 20,
+},
+itemleft :{
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+},
+square :{
+    width: 24, 
+    height: 24, 
+    backgroundColor: '#55BCF6',
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+},
+itemText :{
+    maxWidth: '80%',
+},
 });
+
+export default Task;
